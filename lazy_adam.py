e147b1aab29e31a91e38b3186818222733407300
davda54 <david.samuel@seznam.cz>
2022-08-11 01:12:08 +0200

    update to PyTorch 1.11.0

--- lazy_adam.py
+++ lazy_adam.py
@@ -1,124 +1,125 @@
-import math
 import torch
+import torch.optim._functional as F
 from torch.optim.optimizer import Optimizer
 
 
-class LazyAdam(Optimizer):
-
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8):
-        if not 0.0 < lr:
+class LazyAdamW(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-2):
+        if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 < eps:
+        if not 0.0 <= eps:
             raise ValueError("Invalid epsilon value: {}".format(eps))
         if not 0.0 <= betas[0] < 1.0:
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=False, maximize=False)
+        super(LazyAdamW, self).__init__(params, defaults)
 
-        defaults = dict(lr=lr, betas=betas, eps=eps)
-        super(LazyAdam, self).__init__(params, defaults)
+    def __setstate__(self, state):
+        super(LazyAdamW, self).__setstate__(state)
 
+    @torch.no_grad()
     def step(self, closure=None):
         """Performs a single optimization step.
 
-        Arguments:
+        Args:
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
         """
-
         loss = None
         if closure is not None:
-            loss = closure()
+            with torch.enable_grad():
+                loss = closure()
 
         for group in self.param_groups:
-            for p in group['params']:
+            dense_params_with_grad, sparse_params_with_grad = [], []
+            dense_grads, sparse_grads = [], []
+            dense_exp_avgs, sparse_exp_avgs = [], []
+            dense_exp_avg_sqs, sparse_exp_avg_sqs = [], []
+            dense_state_steps, sparse_state_steps = [], []
+            beta1, beta2 = group['betas']
 
+            for p in group['params']:
                 if p.grad is None:
                     continue
-                grad = p.grad.data
 
-                if grad.is_sparse:
-                    self.sparse_step(group, p, grad)
+                if p.grad.is_sparse:
+                    sparse_params_with_grad.append(p)
+                    sparse_grads.append(p.grad)
+
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        # Exponential moving average of gradient values
+                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        # Exponential moving average of squared gradient values
+                        state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    sparse_exp_avgs.append(state['exp_avg'])
+                    sparse_exp_avg_sqs.append(state['exp_avg_sq'])
+
+                    # update the steps for each param group update
+                    state['step'] += 1
+                    # record the step after step update
+                    sparse_state_steps.append(state['step'])
+
                 else:
-                    self.dense_step(group, p, grad)                
+                    dense_params_with_grad.append(p)
+                    if p.grad.is_sparse:
+                        raise RuntimeError('AdamW does not support sparse gradients')
+                    dense_grads.append(p.grad)
+
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        # Exponential moving average of gradient values
+                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        # Exponential moving average of squared gradient values
+                        state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    dense_exp_avgs.append(state['exp_avg'])
+                    dense_exp_avg_sqs.append(state['exp_avg_sq'])
+
+                    # update the steps for each param group update
+                    state['step'] += 1
+                    # record the step after step update
+                    dense_state_steps.append(state['step'])
+
+            if len(dense_params_with_grad) > 0:
+                F.adamw(
+                    dense_params_with_grad,
+                    dense_grads,
+                    dense_exp_avgs,
+                    dense_exp_avg_sqs,
+                    [],
+                    dense_state_steps,
+                    amsgrad=False,
+                    beta1=beta1,
+                    beta2=beta2,
+                    lr=group['lr'],
+                    weight_decay=group['weight_decay'],
+                    eps=group['eps'],
+                    maximize=False
+                )
+
+            if len(sparse_params_with_grad) > 0:
+                F.sparse_adam(
+                    sparse_params_with_grad,
+                    sparse_grads,
+                    sparse_exp_avgs,
+                    sparse_exp_avg_sqs,
+                    sparse_state_steps,
+                    beta1=beta1,
+                    beta2=beta2,
+                    lr=group['lr'],
+                    eps=group['eps']
+                )
 
         return loss
-
-
-    def sparse_step(self, group, param, grad):
-        state = self.state[param]
-
-        # State initialization
-        if len(state) == 0:
-            state['step'] = 0
-            # Exponential moving average of gradient values
-            state['exp_avg'] = torch.zeros_like(param.data)
-            # Exponential moving average of squared gradient values
-            state['exp_avg_sq'] = torch.zeros_like(param.data)
-
-        state['step'] += 1
-
-        grad = grad.coalesce()  # the update is non-linear so indices must be unique
-        grad_indices = grad._indices()
-        grad_values = grad._values()
-        size = grad.size()
-
-        def make_sparse(values):
-            constructor = grad.new
-            if grad_indices.dim() == 0 or values.dim() == 0:
-                return constructor().resize_as_(grad)
-            return constructor(grad_indices, values, size)
-
-        exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-        beta1, beta2 = group['betas']
-
-        # Decay the first and second moment running average coefficient
-        #      old <- b * old + (1 - b) * new
-        # <==> old += (1 - b) * (new - old)
-        old_exp_avg_values = exp_avg.sparse_mask(grad)._values()
-        exp_avg_update_values = grad_values.sub(old_exp_avg_values).mul_(1 - beta1)
-        exp_avg.add_(make_sparse(exp_avg_update_values))
-        old_exp_avg_sq_values = exp_avg_sq.sparse_mask(grad)._values()
-        exp_avg_sq_update_values = grad_values.pow(2).sub_(old_exp_avg_sq_values).mul_(1 - beta2)
-        exp_avg_sq.add_(make_sparse(exp_avg_sq_update_values))
-
-        # Dense addition again is intended, avoiding another sparse_mask
-        numer = exp_avg_update_values.add_(old_exp_avg_values)
-        exp_avg_sq_update_values.add_(old_exp_avg_sq_values)
-        denom = exp_avg_sq_update_values.sqrt_().add_(group['eps'])
-        del exp_avg_update_values, exp_avg_sq_update_values
-
-        bias_correction1 = 1 - beta1 ** state['step']
-        bias_correction2 = 1 - beta2 ** state['step']
-        step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
-
-        param.data.add_(make_sparse(-step_size * numer.div_(denom)))
-
-    
-    def dense_step(self, group, param, grad):
-        state = self.state[param]
-
-        # State initialization
-        if len(state) == 0:
-            state['step'] = 0
-            # Exponential moving average of gradient values
-            state['exp_avg'] = torch.zeros_like(param.data)
-            # Exponential moving average of squared gradient values
-            state['exp_avg_sq'] = torch.zeros_like(param.data)
-
-        exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-        beta1, beta2 = group['betas']
-
-        state['step'] += 1
-        bias_correction1 = 1 - beta1 ** state['step']
-        bias_correction2 = 1 - beta2 ** state['step']
-
-        # Decay the first and second moment running average coefficient
-        exp_avg.mul_(beta1).add_(1 - beta1, grad)
-        exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-        
-        denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
-
-        step_size = group['lr'] / bias_correction1
-
-        param.data.addcdiv_(-step_size, exp_avg, denom)
